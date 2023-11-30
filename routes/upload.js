c50e1d6b232985c6e14c0785243850a7460e04be
Lani Jung <lani009@naver.com>
2023-11-30 21:11:19 +0900

    add feature: upload files

--- routes/upload.js
+++ routes/upload.js
@@ -5,6 +5,11 @@ const fs = require('fs');
 const path = require('path');
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "views", "upload.html"));
+  });
+  
+
 // 파일 저장을 위한 Multer 설정
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -24,21 +29,20 @@ router.post('/', upload.single('file'), (req, res) => {
     const fileItem2 = req.body.fileItem2;
     const fileItem3 = req.body.fileItem3;
     const fileName = file.originalname;
-    console.log("hello");
     // 파일 내용 읽기
     const filePath = path.join(__dirname, '..', 'uploads', fileName);
     const fileContent = fs.readFileSync(filePath, 'utf8');
     console.log(req);
     // 파일 정보를 데이터베이스에 저장
     const query = 'INSERT INTO system_a_files (file_item1, file_item2, file_item3, file_name, file_content) VALUES (?, ?, ?, ?, ?)';
-    db.query(query, [fileItem1, fileItem2, fileItem3, fileName, fileContent], (err) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).send('Error saving file info to database');
-        }
-        console.log('File info saved to database.');
-        res.send('File uploaded and info saved to database.');
-    });
+    // db.query(query, [fileItem1, fileItem2, fileItem3, fileName, fileContent], (err) => {
+    //     if (err) {
+    //         console.error(err);
+    //         return res.status(500).send('Error saving file info to database');
+    //     }
+    //     console.log('File info saved to database.');
+    //     res.send('File uploaded and info saved to database.');
+    // });
 });
 
 module.exports = router;
