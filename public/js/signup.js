0fc653a862ce6cf0d9fb6d7276a1724cb72eede7
Na Jongwoo <njw1204@naver.com>
2023-12-04 18:52:28 +0900

    feat: add multiselect for systems on signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -12,3 +12,15 @@ $(document).on('input', '#phone', function (e) {
 
   this.dataset.prevValue = this.value;
 });
+
+$(function () {
+  $('#signup-systems-select').removeClass('d-none');
+
+  new SlimSelect({
+    select: '#signup-systems-select',
+    settings: {
+      openPosition: 'down',
+      placeholderText: '여기를 클릭해 검색하세요',
+    },
+  });
+});
